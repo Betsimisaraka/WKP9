2fd9c6ecfe7c6a42eacb48a3da172ff7e8bd38f4
Anita RASOA <anita.ras@onja.org>
2020-08-29 11:27:37 +0300

    Showing the list of the studio ghibli movies

---
+++ script.js
@@ -0,0 +1,68 @@
+const listElement = document.querySelector('.movie_lists')
+const proxy = 'https://ghibliapi.herokuapp.com/films';
+
+async function fetchMovies() {
+    const res = await fetch(`${proxy}`);
+    const data = await res.json();
+    return data;
+}
+
+async function fetchAndDisplay() {
+    const movies = await fetchMovies();
+    displayMovies(movies);
+}
+
+function displayMovies(movies) {
+    const scoreSorted = movies.sort((a, b) => (b.rt_score - a.rt_score));
+    const html = scoreSorted.map(movie => {
+        return `
+        <li class="container" data-id="${movie.id}">
+            <h2>${movie.title}</h2>
+            <small class="date">Release-date: ${movie.release_date}</small>
+            <p class="description">${movie.description}</p>
+            <p class="director">Director: ${movie.director}</p>
+            <p class="producer">Producer: ${movie.producer}</p>
+            <small class="score">rt-score:${movie.rt_score}</small>
+        </li>
+        `;
+    }).join('');
+   listElement.innerHTML = html;
+    console.log(html);
+}
+
+fetchAndDisplay();
+
+//const listElement = document.querySelector('.movie_lists')
+
+// async function fetchMovies() {
+//     const response = await fetch('https://ghibliapi.herokuapp.com/films', {
+//             headers: {
+//                 Accept: 'application/json',
+//             }
+//         });
+//         console.log(response);
+//         const data = await response.json();
+//         return data;
+// }
+
+// async function displayMovies() {
+//     const results = await fetchMovies();
+//     const scoreSorted = results.sort((a, b) => (b.rt_score - a.rt_score));
+//     const html = scoreSorted.map(movie => {
+//         return `
+//         <li class="container" data-id="${movie.id}">
+//             <h2>${movie.title}</h2>
+//             <small class="date">Release-date: ${movie.release_date}</small>
+//             <p class="description">${movie.description}</p>
+//             <p class="director">Director: ${movie.director}</p>
+//             <p class="producer">Producer: ${movie.producer}</p>
+//             <small class="score">rt-score:${movie.rt_score}</small>
+//         </li>
+//         `;
+//     }).join('');
+//    listElement.innerHTML = html;
+//     console.log(html);
+// } 
+
+// displayMovies();
+
