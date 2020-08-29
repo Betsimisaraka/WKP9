5ce2c5c84598fc3727a1872f6d2499133f3e54ee
Anita RASOA <anita.ras@onja.org>
2020-08-29 14:19:01 +0300

    Updated the readme and putting some comments in every function

--- readme.md
+++ readme.md
@@ -1,19 +1,19 @@
 # WKP9
 
-Hey team!
-After a major graded project, let's do something different, and a little bit more straightforward.
-Today we’re going to build a list of the Studio Ghibli Movies, ranked from best to worst. If you haven’t seen any of those, I suggest you to look on the Onja hard drive! (after completing the project of course). They are amazing ✨
+1. ## In a few sentences, explain the structure of your project .
 
-The website will look similar to this :
+- First of all, I fetch the films from the URL that is given to us.
+- Display the response of the movies that I have got from the data.
+- I created a html to display the films.
+- I append the html to the DOM. 
+1. ## If you had more time, what area of your work would you improve?
 
-![assets/ghibli.png](assets/ghibli.png)
+    If I had more time I would improve my css and add more attribute in the data.
 
-You will fetch the data from this URL [https://ghibliapi.herokuapp.com/films](https://ghibliapi.herokuapp.com/films)
+1. ## Did you learn anything new while working on this project?
 
-and display the result in the HTML. The film needs to be sorted depending on their rt_score!
-
-There is a lot of data in a film object, use whatever attribute you need.
-
-Feel free to add some beautiful styling to your website when you're done fetching and sorting the data.
-
-Good luck 💻
+    Yes, I learn how to get a response from the data and to append it in the html.
+1. ## What was the most challenging part for you?
+    The most challenging part for me is the way that I can use the async await.
+1. ## Would you like a new explanation about a specific topic?
+    Yes, I need more explaination on using the promise and the async await. I am not fully understand when should we use them.
--- script.js
+++ script.js
@@ -1,19 +1,25 @@
+//grab the ul from the html.
 const listElement = document.querySelector('.movie_lists')
 const url = 'https://ghibliapi.herokuapp.com/films';
 
+//fetch the data from the url to get all of the films.
 async function fetchMovies() {
     const res = await fetch(`${url}`);
     const data = await res.json();
     return data;
 }
 
+//display the films
 async function handleMovies() {
     const movies = await fetchMovies();
     displayMovies(movies);
 }
 
+//display the films by creating html
 function displayMovies(movies) {
+    //sort the movies from the best to the worst rt_score.
     const scoreSorted = movies.sort((a, b) => (b.rt_score - a.rt_score));
+    //mapping through the movies to access all of them.
     const html = scoreSorted.map(movie => {
         return `
         <li class="container" data-id="${movie.id}">
@@ -26,43 +32,8 @@ function displayMovies(movies) {
         </li>
         `;
     }).join('');
+    //append the html in the DOM.
    listElement.innerHTML = html;
-    console.log(html);
 }
 
 handleMovies();
-
-//const listElement = document.querySelector('.movie_lists')
-
-// async function fetchMovies() {
-//     const response = await fetch('https://ghibliapi.herokuapp.com/films', {
-//             headers: {
-//                 Accept: 'application/json',
-//             }
-//         });
-//         console.log(response);
-//         const data = await response.json();
-//         return data;
-// }
-
-// async function displayMovies() {
-//     const results = await fetchMovies();
-//     const scoreSorted = results.sort((a, b) => (b.rt_score - a.rt_score));
-//     const html = scoreSorted.map(movie => {
-//         return `
-//         <li class="container" data-id="${movie.id}">
-//             <h2>${movie.title}</h2>
-//             <small class="date">Release-date: ${movie.release_date}</small>
-//             <p class="description">${movie.description}</p>
-//             <p class="director">Director: ${movie.director}</p>
-//             <p class="producer">Producer: ${movie.producer}</p>
-//             <small class="score">rt-score:${movie.rt_score}</small>
-//         </li>
-//         `;
-//     }).join('');
-//    listElement.innerHTML = html;
-//     console.log(html);
-// } 
-
-// displayMovies();
-
